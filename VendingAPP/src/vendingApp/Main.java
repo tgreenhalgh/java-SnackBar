72b49ddc9c7ad464d97dcdb7f84e817b6018da9e
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-02-06 15:59:45 -0800

    accidentally deleted addCash. put it back in

--- VendingAPP/src/vendingApp/Main.java
+++ VendingAPP/src/vendingApp/Main.java
@@ -29,6 +29,7 @@ public class Main {
     // Print quantity of snack 4.
     System.out.println("Quantity of snack 4 left " + s4.getQuantity());
     // Customer 1 finds $10. Print Customer 1 Cash on hand.
+    // addCash returns the new total
     System.out.println("Cash on hand for customer 1 $" + c1.addCash(10));
     // Customer 1 buys 1 of snack 2.
     c1.buySnack(1, s2);
@@ -38,7 +39,7 @@ public class Main {
     System.out.println("Quantity of snack 2 left " + s2.getQuantity());
     // Snack 3 gets 12 more. Print quantity of snack 3.
     // addQuantity returns the new total
-    System.out.println("Quantity of snack 3 left " + s3.getQuantity());
+    System.out.println("Quantity of snack 3 left " +s3.addQuantity(12));
     // Customer 2 buys 3 of snack 3.
     c2.buySnack(3, s3);
     // Print Customer 2 Cash on hand.
